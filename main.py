1abf32c3fd47c9b3d19f3e3e09016bbfa81c7683
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-05-11 21:04:17 +0530

    added_Comment

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import pyttsx3
+import pyttsx3                      # text-to-speech conversion library and it works offline
 
-engine=pyttsx3.init()
+engine=pyttsx3.init()                         # object created 
 
-for voice in engine.getProperty("voices"):
+for voice in engine.getProperty("voices"):    
     pass
 
 voices=engine.getProperty("voices")
-
 engine.setProperty("voice",voices[1].id)
 
-def Speak(Audio):
-    engine.say(Audio)
-    engine.runAndWait()
+def Speak(Audio):               # creating a function that helps us to convert a text to speech. 
+    engine.say(Audio)           # the function is used to speak our program
+    engine.runAndWait()         # used to wait for our program for the next execution,
+
 
 text=input("Enter your text : ")
 
